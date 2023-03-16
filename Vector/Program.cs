56771f4dc25fcca3b20fcf2cdb2a4fc71f615aac
rakitinva <rakitinva@206D-1>
2023-03-16 15:48:40 +0700

    Добавьте файлы проекта.

---
+++ Vector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vector
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vector vector = new Vector(3, 4);
+            Console.WriteLine(vector.ToString());
+            vector.X = 0;
+            vector.Y = -1;
+            Console.WriteLine(vector.ToString());
+            vector = new Vector(9, 40);
+            Console.WriteLine(vector.ToString());
+            Console.WriteLine(new Vector(0, 0).ToString());
+        }
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vector
+{
+    public class Vector
+    {
+        private double x;
+        private double y;
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            Length = Math.Sqrt(x * x + y * y);
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; Length = Math.Sqrt(x * x + y * y); }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; Length = Math.Sqrt(x * x + y * y); }
+        }
+
+        public double Length { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1}) with length: {2}", X, Y, Length);
+        }
+    }
+}
